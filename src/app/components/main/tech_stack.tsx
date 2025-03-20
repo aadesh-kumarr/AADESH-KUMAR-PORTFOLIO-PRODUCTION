10249be7877e4844ef617ce9ed8efab79e3df239
Aadesh Kumar <116622502+aadesh-kumarr@users.noreply.github.com>
2025-03-20 14:03:30 +0530

    tech stack added

--- src/app/components/main/tech_stack.tsx
+++ src/app/components/main/tech_stack.tsx
@@ -8,9 +8,11 @@ const techStack: TechStackItem[] = [
   { id: 1, label: "Next Js", logo: <img src={"/svgs/tech_stack/nextjs.svg"} alt="Next Js Logo" className="h-6 w-6" /> },
   { id: 2, label: "React Js", logo: <img src={'/svgs/tech_stack/reactjs.svg'} alt="React Js Logo" className="h-6 w-6" /> },
   { id: 3, label: "MongoDB", logo: <img src={'/svgs/tech_stack/mongodb.svg'} alt="MongoDB Logo" className="h-6 w-6" /> },
-  { id: 4, label: "Prisma", logo: <img src={'/svgs/tech_stack/prisma.svg'} alt="Prisma Logo" className="h-6 w-6" /> },
-  { id: 5, label: "Shad CN", logo: <img src={'/svgs/tech_stack/shadcn-ui-seeklogo.png'} alt="Shad CN Logo" className="h-6 w-6" /> },
-  { id: 6, label: "Data tables", logo: <img src={'/svgs/tech_stack/database-table.svg'} alt="Data Tables Logo" className="h-6 w-6" /> },
+  { id: 4, label: "Type Script", logo: <img src={'/svgs/tech_stack/typescript.png'} alt="MongoDB Logo" className="h-6 w-6" /> },
+  { id: 5, label: "Auth Js", logo: <img src={'/svgs/tech_stack/auth.png'} alt="MongoDB Logo" className="h-6 w-6" /> },
+  { id: 6, label: "Prisma", logo: <img src={'/svgs/tech_stack/prisma.svg'} alt="Prisma Logo" className="h-6 w-6" /> },
+  { id: 7, label: "Shad CN", logo: <img src={'/svgs/tech_stack/shadcn-ui-seeklogo.png'} alt="Shad CN Logo" className="h-6 w-6" /> },
+  { id: 8, label: "Data tables", logo: <img src={'/svgs/tech_stack/database-table.svg'} alt="Data Tables Logo" className="h-6 w-6" /> },
 ];
 
 export default function Tech_Stack() {
